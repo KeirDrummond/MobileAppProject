e4a4c1034135473a53491f5e35e0b2d8e4f95b76
KeirDrummond <k.drummond2@uni.brighton.ac.uk>
2020-04-13 14:06:35 +0100

    Item view

--- app/src/main/java/com/example/ratemypet/HomeFragment.java
+++ app/src/main/java/com/example/ratemypet/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.ratemypet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,22 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenItemActivity();
+            }
+        });
+
+        return view;
     }
+
+    public void OpenItemActivity() {
+        Intent intent = new Intent(this.getContext(), ItemViewActivity.class);
+        startActivity(intent);
+    }
+
 }
